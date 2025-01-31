a6e650d3576f0b53c6c31aa8246a3f5be31d2fb4
notalihassan <alihassan92199@gmail.com>
2025-02-01 00:17:55 +0500

    first commit

---
+++ Assignment_01/cvusingprintf.c
@@ -0,0 +1,99 @@
+#include <stdio.h>
+int main(){
+   
+
+    //    CV using printf()
+
+    printf("============================================================================================ \n");
+    printf("||                                                                                        || \n");
+    printf("||   Name:                                                                                || \n");
+    printf("||  ------                                                                                || \n");
+    printf("||          ____________________________________________________________________          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |                            Ali Hassan                            |          || \n");
+    printf("||          |__________________________________________________________________|          || \n");
+    printf("||                                                                                        || \n");
+    printf("||   About me:                                                                            || \n");
+    printf("||  ----------                                                                            || \n");
+    printf("||          ____________________________________________________________________          || \n");
+    printf("||          |                                                                  |          || \n");    
+    printf("||          |          Here I am Ali Hassan with advanced skills in computer   |          || \n");
+    printf("||          |  technologies and innovative problem-solving. My expertise and   |          || \n");
+    printf("||          |  passion for staying ahead of industry trends make me a valuable |          || \n");
+    printf("||          |  asset in driving efficiency and performance in any tech-driven  |          || \n");
+    printf("||          |  environment.                                                    |          || \n");
+    printf("||          |__________________________________________________________________|          || \n");
+    printf("||                                                                                        || \n");
+    printf("||   Personal Information:                                                                || \n");
+    printf("||  ----------------------                                                                || \n");
+    printf("||          ____________________________________________________________________          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |         Contact:            0316-4658515                         |          || \n");
+    printf("||          |         Email:              engr.alihassan92199@gmail.com        |          || \n");
+    printf("||          |         LinkedIn:           @notalihassan                        |          || \n");
+    printf("||          |         Address:            Airport road, Waqas Town, Multan     |          || \n");
+    printf("||          |__________________________________________________________________|          || \n");
+    printf("||                                                                                        || \n");
+    printf("||   Education:                                                                           || \n");
+    printf("||  -----------                                                                           || \n");
+    printf("||          ____________________________________________________________________          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |   Graduation:                                                    |          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |     Institute: Bahauddin Zakariya University, Multan             |          || \n");
+    printf("||          |     Degree:    Bachelor's of Computer Engineering                |          || \n");
+    printf("||          |     Marks:     3.8 GPA                                           |          || \n");
+    printf("||          |     Duration:  2024-2028                                         |          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |==================================================================|          || \n");
+    printf("||          |   Intermediate:                                                  |          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |     Institute: Nishat College of Science, Multan                 |          || \n");
+    printf("||          |     Degree:    Pre-Engineering                                   |          || \n");
+    printf("||          |     Marks:     80 percent                                        |          || \n");
+    printf("||          |     Duration:  2021-2023                                         |          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |==================================================================|          || \n");
+    printf("||          |   Matriculation:                                                 |          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |     Institute: CDG Boys High School, Lahore                      |          || \n");
+    printf("||          |     Degree:    Computer Science                                  |          || \n");
+    printf("||          |     Marks:     97 percent                                        |          || \n");
+    printf("||          |     Duration:  2019-2021                                         |          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |__________________________________________________________________|          || \n");
+    printf("||                                                                                        || \n");
+    printf("||   Skills:                                                                              || \n");
+    printf("||  --------                                                                              || \n");
+    printf("||          ____________________________________________________________________          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |    *  HTML & CSS                                                 |          || \n");
+    printf("||          |    *  Basics of C & C++                                          |          || \n");
+    printf("||          |    *  MS OFFICE                                                  |          || \n");
+    printf("||          |    *  Photographer                                               |          || \n");
+    printf("||          |    *  Video Editor                                               |          || \n");
+    printf("||          |__________________________________________________________________|          || \n");
+    printf("||                                                                                        || \n");
+    printf("||   Language:                                                                            || \n");
+    printf("||  ----------                                                                            || \n");
+    printf("||          ____________________________________________________________________          || \n");
+    printf("||          |                                                                  |          || \n");
+    printf("||          |    *  English                                                    |          || \n");
+    printf("||          |    *  Urdu                                                       |          || \n");
+    printf("||          |    *  Punjabi                                                    |          || \n");
+    printf("||          |__________________________________________________________________|          || \n");
+    printf("||                                                                                        || \n");
+    printf("============================================================================================ \n");
+
+    
+    
+    
+    
+    
+
+    
+
+    
+
+
+}
---
+++ Assignment_01/cvusingprintf1.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main(){
+   
+
+    //    CV using printf()
+
+    printf("==================================================================== \n");
+    printf("|| Name:                                                          || \n");
+    printf("||                            Ali Hassan                          || \n");
+    printf("||                                                                || \n");
+    printf("==================================================================== \n");
+
+    // about section 
+
+    printf("==================================================================== \n");
+    printf("|| About Me:                                                      || \n");
+    printf("||          Here I am Ali Hassan with advanced skills in computer || \n");
+    printf("|| technologies and innovative problem-solving. My expertise and  || \n");
+    printf("|| passion for staying ahead of industry trends make me a valuable|| \n");
+    printf("|| asset in driving efficiency and performance in any tech-driven || \n");
+    printf("|| environment.                                                   || \n");
+    printf("==================================================================== \n");
+
+    // Personal Information
+
+    printf("==================================================================== \n");
+    printf("|| Pesonal Information:                                           || \n");
+    printf("||                                                                || \n");
+    printf("||         Contact:            0316-4658515                       || \n");
+    printf("||         Email:              engr.alihassan92199@gmail.com      || \n");
+    printf("||         LinkedIn:           @notalihassan                      || \n");
+    printf("||         Address:            Airport road, Waqas Town, Multan   || \n");
+    printf("==================================================================== \n");
+
+
+}
