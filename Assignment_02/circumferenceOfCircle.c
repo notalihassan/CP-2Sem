5ebc912877b25b08919dc36b70015d10205e8244
notalihassan <alihassan92199@gmail.com>
2025-02-04 22:48:36 +0500

    add file

---
+++ Assignment_02/circumferenceOfCircle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+int main(){
+
+    // Circumference of Cicle = 2 * pi * r
+
+    float radius = 3, circumference;
+    circumference = 2 * M_PI * radius;
+
+    printf("Circumference of Circle : %.2f",circumference );
+
+    return 0;
+
+
+}
